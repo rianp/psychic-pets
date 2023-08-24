9e480a6e248b627fe3dd86b60e6f1e0db71f9cee
rian <rianchristopher@gmail.com>
2023-08-24 13:48:02 -0700

    refactor query   - delete mutation functionality

--- PsychicPets/Pets/PetsSchema.cs
+++ PsychicPets/Pets/PetsSchema.cs
@@ -7,6 +7,5 @@ public class PetsSchema : Schema
     public PetsSchema(IServiceProvider serviceProvider) : base(serviceProvider)
     {
         Query = serviceProvider.GetRequiredService<PetsQuery>();
-        Mutation = serviceProvider.GetRequiredService<PetsMutation>();
     }
 }
