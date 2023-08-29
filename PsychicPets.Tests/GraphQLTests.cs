f3882c6c1750263afe1785d3b02f338395e0e3bd
rian <rianchristopher@gmail.com>
2023-08-29 12:05:41 -0700

    fix whitespace

--- PsychicPets.Tests/GraphQLTests.cs
+++ PsychicPets.Tests/GraphQLTests.cs
@@ -2,5 +2,4 @@ namespace DefaultNamespace;
 
 public class GraphQLTests
 {
-    
 }
--- PsychicPets/Models/Pet.cs
+++ PsychicPets/Models/Pet.cs
@@ -4,7 +4,7 @@ namespace PsychicPets.Models;
 
 public class Pet
 {
-  public int Id { get; set; }
-  [Required]
-  public string Name { get; set; }
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
 }
